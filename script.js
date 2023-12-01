39f04f867bc1d176446aa428ee7156ac2fbde334
Jacinto Wong <contact@jacinto.design>
2023-11-30 21:23:10 -0500

    added transitions to section elements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// About section
 document.addEventListener('DOMContentLoaded', () => {
+  // About Observer
   const phrases = document.querySelectorAll('#about .phrase');
 
   const observerOptions = {
@@ -42,4 +42,51 @@ document.addEventListener('DOMContentLoaded', () => {
   galleryItems.forEach(item => {
     galleryObserver.observe(item);
   });
+
+  // Blog Observer
+  const blogArticles = document.querySelectorAll('#blog .featured-article, #blog .article');
+
+  const blogObserverOptions = {
+    root: null, // viewport
+    threshold: 0.3 // adjust as needed, 0.1 means 10% of the item in the viewport
+  };
+
+  const blogObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Add class to start animation
+        entry.target.classList.add('fadeInUp');
+      } else {
+        // Remove class to reset animation
+        entry.target.classList.remove('fadeInUp');
+      }
+    });
+  }, blogObserverOptions);
+
+  blogArticles.forEach(item => {
+    blogObserver.observe(item);
+  });
+
+  // Contact Observer
+  const contactItems = document.querySelectorAll('#contact > div');
+
+  const contactObserverOptions = {
+    root: null, // viewport
+    threshold: 0.3 // adjust as needed
+  };
+
+  const contactObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('fadeInUp');
+      } else {
+        entry.target.classList.remove('fadeInUp');
+      }
+    });
+  }, contactObserverOptions);
+
+  contactItems.forEach(item => {
+    contactObserver.observe(item);
+  });
+
 });
