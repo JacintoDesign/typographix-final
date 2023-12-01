3152d3af1361be07dc755aac86945c1eae99b8a1
Jacinto Wong <contact@jacinto.design>
2023-11-30 19:58:35 -0500

    first draft

--- script.js
+++ script.js
@@ -1,73 +1,45 @@
-// Function to check if page is scrolled and adjust the logo size
-function checkScroll() {
-    const navbar = document.getElementById("navbar");
-    const logo = document.getElementById("logo");
-    let scrollPosition = window.scrollY;
-    
-    // Add/remove 'scrolled' class based on scroll position
-    if (scrollPosition > 20) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-    
-    // Calculate new font size based on scroll position
-    let newSize = 3 - (scrollPosition * 0.03); // Decrease by 0.03 rem for every 1px scrolled
-    
-    // Clamping the font size between 1.5rem and 3rem
-    newSize = Math.max(1.5, newSize);
-    newSize = Math.min(3, newSize);
-    
-    logo.style.fontSize = newSize + "rem";
-}
+// About section
+document.addEventListener('DOMContentLoaded', () => {
+  const phrases = document.querySelectorAll('#about .phrase');
 
-// Event listener for scroll event
-window.addEventListener('scroll', checkScroll);
+  const observerOptions = {
+    root: null, // viewport
+    threshold: 1 // Trigger halfway through the element's visibility
+  };
 
-// Dark Mode
-const themeSwitcher = document.getElementById('theme-switcher');
+  const phraseObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  }, observerOptions);
 
-function updateThemeIcon(isDarkMode) {
-  themeSwitcher.children[0].classList.replace(isDarkMode ? 'fa-sun' : 'fa-moon', isDarkMode ? 'fa-moon' : 'fa-sun');
-}
+  phrases.forEach(phrase => {
+    phraseObserver.observe(phrase);
+  });
 
-// Determine if dark mode is preferred
-function prefersDarkMode() {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
+  // Gallery Observer
+  const galleryItems = document.querySelectorAll('#gallery .image-box');
 
-// Set the theme based on the preference
-function setThemeBasedOnPreference() {
-  const isDarkMode = prefersDarkMode();
-  document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : 'light');
-  updateThemeIcon(isDarkMode);
-}
+  const galleryObserverOptions = {
+    root: null, // viewport
+    threshold: 1 // adjust as needed
+  };
 
-// Switch Theme manually
-function switchTheme() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-  document.documentElement.setAttribute('data-theme', newTheme);
-  localStorage.setItem('theme', newTheme);
-  updateThemeIcon(newTheme === 'dark');
-}
+  const galleryObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+      } else {
+        entry.target.classList.remove('active');
+      }
+    });
+  }, galleryObserverOptions);
 
-// Event Listener
-themeSwitcher.addEventListener('click', switchTheme);
-
-// Initialize Theme
-function initializeTheme() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme) {
-      document.documentElement.setAttribute('data-theme', savedTheme);
-      updateThemeIcon(savedTheme === 'dark');
-  } else {
-      setThemeBasedOnPreference();
-  }
-}
-
-// Listen for system theme changes
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', setThemeBasedOnPreference);
-
-// Initialize theme when the script loads
-initializeTheme();
+  galleryItems.forEach(item => {
+    galleryObserver.observe(item);
+  });
+});
