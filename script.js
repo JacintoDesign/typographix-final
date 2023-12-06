5fd98929058fa011136ab13ae7954a2b147c0119
Jacinto Wong <contact@jacinto.design>
2023-12-06 15:21:52 -0500

    added form submission

--- script.js
+++ script.js
@@ -46,3 +46,33 @@ function hideMenu() {
     listItem.classList.remove('active');
   });
 }
+
+// Form Submission ---------------------------------
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var form = this;
+  var formData = new FormData(form);
+
+  fetch(form.action, {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then(response => {
+      if (response.ok) {
+        form.reset();
+        var toast = document.getElementById("toast");
+        toast.classList.add("show");
+        setTimeout(function(){ toast.classList.remove("show"); }, 10000);        
+      } else {
+        // Handle errors here
+        alert('Form submission failed!');
+    }
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+});
